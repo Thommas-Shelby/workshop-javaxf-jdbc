f8a358388fb0f6acc04cf56a7f3a1055554971c9
Thommas Shelby <jordan-360@live.com>
2022-10-12 21:32:33 -0300

    About View

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module workshop {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.graphics, javafx.fxml;
